f230322afb0ce66bed6eb8bd1778078cc083af37
Sameddin-Asadov <id.s.asadov@gmail.com>
2024-11-03 16:51:19 +0400

    new jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Index from './Index'
-import Parent from './Parent'
+
+import Login from './assets/Login'
 
 function App() {
 
   return (
        <div>
-        
+        <Login/>
+        <hr />
+        <Login/>
        </div>
   )
 }
---
+++ src/assets/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <div>Login</div>
+        <input type="text" />
+      </div>
+      <div>
+        <div>Password</div>
+        <input type="password" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
